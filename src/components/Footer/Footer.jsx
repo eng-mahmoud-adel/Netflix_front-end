b4334ff231c4d7d225f14be9c2faa48e5634a4ad
Sarah <sarah.ali2810@gmail.com>
2021-04-01 04:14:19 +0200

    creating footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import ListElements from './ListElements'; 
+
+function Footer(props) {
+    console.log(props.color);
+    return (
+
+    <>
+        <div style={{backgroundColor:props.color, padding: '50px 13.9vw'}}>
+            <p>
+                <NavLink className="text-muted" to="#">
+                    Questions? Contact us.
+                </NavLink>
+            </p>
+
+            <ul className="row m-0 pl-0">
+                <ListElements>
+                    {props.children}
+                </ListElements>
+            </ul>
+        </div>
+    </>
+    );
+}
+
+export default Footer;
