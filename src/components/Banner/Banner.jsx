a21c2de68bcc43422cbd57a4703476aca5afcea6
zaki <ahmed.zaki1998@outlook.com>
2021-04-05 19:27:42 +0200

    add banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import "./test.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Navbar'
+function Banner() {
+  return (
+    <>
+    <div className="banner container-fluid">
+    <div className="banner_top_fade"></div>
+      <div class="row">
+        <div className="banner_info offset-lg-1 offset-md-1 offset-sm-1 col-lg-5 col-md-7 col-sm-9 col-9 offset-1">
+          <h1 className="banner_title">Money Heist</h1>
+          <div className="">
+            <button className="banner_button">Play</button>
+            <button className="banner_button">More Info</button>
+          </div>
+          <p className="banner_description">
+            A criminal mastermind who goes by "The Professor" has a plan to pull
+            off the biggest heist in recorded history -- to print billions of
+            euros in the Royal Mint of Spain.
+          </p>
+        </div>
+      </div>
+      <div className="banner_fade"></div>
+    </div>
+    </>
+  );
+}
+
+export default Banner;
