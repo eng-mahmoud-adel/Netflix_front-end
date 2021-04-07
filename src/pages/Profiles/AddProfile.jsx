260cfa7eaff13d7fed6d33baa34e0dfc0641203f
Sarah <sarah.ali2810@gmail.com>
2021-04-07 03:35:07 +0200

    profiles pages

---
+++ src/pages/Profiles/AddProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AddProfileForm from './../../components/Forms/AddProfileForm';
+
+
+const CreateProfile = () => {
+    return ( 
+        <>
+        <div style={{backgroundColor:"black"}}>
+            <div className="col-12 col-md-9 col-lg-10 col-xl-10 mx-auto py-5" style={{height:"815px",backgroundColor:"black",color:'white'}}>
+                <AddProfileForm /> 
+            </div>
+        </div> 
+        </>
+     );
+}
+ 
+export default CreateProfile;
