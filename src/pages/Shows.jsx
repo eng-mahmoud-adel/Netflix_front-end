06fed926a69f78c3d7a43cb64bd6aa21a3ea966c
zaki <ahmed.zaki1998@outlook.com>
2021-04-12 21:01:43 +0200

    pages redirect to login if no token , send token with all requests

--- src/pages/Shows.jsx
+++ src/pages/Shows.jsx
@@ -1,14 +1,21 @@
 import Banner from '../components/Banner/Banner'
 import Navbar from '../components/Navbar/Navbar'
 import SeriesDashboard from '../components/Dashboard/SeriesDashboard';
+import { Redirect } from 'react-router-dom';
 
 function Shows() {
     return (
+        <>
+            {localStorage.token? 
         <div>
             <Navbar/>
             <Banner/>
 			<SeriesDashboard/>
         </div>
+        : 
+        <Redirect to="/login"/>
+    }
+</>
     );
 }
   
