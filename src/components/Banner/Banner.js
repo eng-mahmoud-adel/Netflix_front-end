75ed93b42dc155846e2386ec0d1d6585f9720a74
zaki <ahmed.zaki1998@outlook.com>
2021-04-14 20:17:17 +0200

    comment buttons

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -9,8 +9,8 @@ function Banner() {
         <div className="banner_info offset-lg-1 offset-md-1 offset-sm-1 col-lg-5 col-md-7 col-sm-9 col-9 offset-1">
           <h1 className="banner_title">Money Heist</h1>
           <div className="">
-            <button className="banner_button">Play</button>
-            <button className="banner_button">More Info</button>
+            {/* <button className="banner_button">Play</button>
+            <button className="banner_button">More Info</button> */}
           </div>
           <p className="banner_description">
             A criminal mastermind who goes by "The Professor" has a plan to pull
