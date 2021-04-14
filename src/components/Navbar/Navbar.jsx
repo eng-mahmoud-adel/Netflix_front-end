1e72c239b770e5a27f5921ce0ded97fb6da9f7d4
Sarah <sarah.ali2810@gmail.com>
2021-04-14 03:41:58 +0200

    navbar profiles update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,6 +25,7 @@ function Navbarr({history}) {
     document.addEventListener("scroll", backgroundHandler)
     return () => window.removeEventListener("scroll",backgroundHandler);
   },[])
+  
     return (
         <Navbar style={{backgroundColor:background}}className="navbar" expand="lg">
           <Navbar.Brand >
@@ -74,7 +75,9 @@ function Navbarr({history}) {
                 <img id='nav-thumbnail'src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png'/>
 
             } id="dropdown-basic">
-        <NavDropdown.Item href="#action/3.1">Profiles</NavDropdown.Item>
+        <Link to="/showprofiles">
+          <NavDropdown.Item href="#action/3.1">Profiles</NavDropdown.Item>
+        </Link>
         <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
